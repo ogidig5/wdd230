f1a58ee4a2817c8d2cf771032fcc5225f2b541bd
ogidig5 <georgeogidi18@gmail.com>
2022-03-22 22:35:09 +0300

    Easy navigation update

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -9,9 +9,8 @@ fetch(apiURL)
     document.querySelector("#condition").textContent = jsObject.weather[0].description;
     document.querySelector("#speed").textContent = jsObject.wind.speed;
     const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-    const desc = jsObject.weather[0].description;
+    // const desc = jsObject.weather[0].description;
     // document.querySelector("#icon-src").textContent = iconsrc;
     document.querySelector("#weather-icon").setAttribute("src", iconsrc);
-    document.querySelector("#weather-icon").setAttribute("alt", desc);
     // document.querySelector("figcaption").textContent = desc;
   });
